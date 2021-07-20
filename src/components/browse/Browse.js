96e0d03c54ea5edbea7fd85bdfedf5f5440768ce
Waqar <nasiimo@venora.no>
2021-07-20 13:59:20 +0200

    removed unused function

--- src/components/browse/Browse.js
+++ src/components/browse/Browse.js
@@ -2,25 +2,6 @@ import React from "react";
 import Banner from "../layout/Banner";
 import axios from "axios";
 
-function getGames() {
-  axios({
-    url: "https://api.igdb.com/v4/games",
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Client-ID": "e3l9ba3mly88sp4zahv6tu1qf3fp9v",
-      Authorization: "Bearer tfukvraqvvpmw12c1g44hwxhjqbw6e",
-    },
-    data: "fields name,genres.name,slug; where screenshots != null;",
-  })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
-
 export default function Browse() {
   function getGames() {
     axios({
